601678c1dd64746c010bc6222771afdfbf43100c
rishabh8887 <rishabhbajpai131@gmail.com>
2026-02-01 07:10:46 +0000

    math&num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 400
+const balance = new Number(100)
+const new_Balance = new String(3488)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(typeof(balance))
+//************ MATHS*************** */
+
+// console.log(Math.abs(-456));
+// console.log(Math.round(54.982647));
+// console.log(Math.floor(2.1));
+// console.log(Math.ceil(4.1));
+// console.log(Math.sqrt(2))
+console.log(Math.round((Math.random()*10)+1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min + 1)) + min)
