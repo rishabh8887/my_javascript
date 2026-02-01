31efce7e442591d0c72c1463a16260dac7d399cc
rishabh8887 <rishabhbajpai131@gmail.com>
2026-02-01 04:16:59 +0000

    datatype summary

---
+++ 01_basics/datatype-Summary.js
@@ -0,0 +1,40 @@
+//Primitive data type - 7 categories
+/**
+ * 1 string
+ * 2 nunmber
+ * 3 boolean
+ * 4 null
+ * 5 undefined
+ * 6 Symbol(used to make something unique)
+ * 7 BigInt(scientific large integer values)
+ */
+
+// Reference type / Non-primitive\
+/**
+ * ARRAY
+ * OBJECTS
+ * FUNCTIONS
+ */
+
+// Javascript is a dynamically type language means we do not need to define the datatype secifically.
+
+//symbol
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const heros =["Shaktiman","naagraj","doga"]
+
+let myObj = {
+    name: "rishabh",
+    age: 22,
+
+}
+
+const myFunction = function(){
+console.log("first function");
+}
+
+console.log(typeof myFunction);
