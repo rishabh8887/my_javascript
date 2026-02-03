dc9dc97c1a6b6326c35a2770b7b233683856c4de
rishabh8887 <rishabhbajpai131@gmail.com>
2026-02-03 12:35:34 +0000

    execution

---
+++ 03_basics/05_execution.js
@@ -0,0 +1,26 @@
+// Global execution context 
+// this - window object in browser context - each have different execution context like node bun browser etc.
+
+/*
+1 - Global Execution Context
+2 Function Execution Context
+3 Eval Execution context
+
+program execution in javascript
+
+1 memory / creation phase
+2 execution phase
+   
+i- global execution created 
+ii- memory phase here all variables are assigned with undefined 
+iii- function comes and definition comes into it 
+iv- code after function is called then it also allocated with the undefined 
+
+second cycle - execution phase
+
+i- real values are allocated 
+ii- 
+
+
+
+*/
