5b7c60809cc9871632cbcac5b47b594ba9ae9dba
rishabh8887 <rishabhbajpai131@gmail.com>
2026-02-04 09:49:43 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while loop
+let index = 0
+while (index<=10) {
+    //console.log(`rishabh ${index}`);
+    index++;
+}
+
+let myArr = ['flash','batman','superman']
+
+let arr = 0
+while(arr<myArr.length){
+    //console.log(`superheros are ${myArr[arr]}`);
+    arr++;
+}
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while( score<=10);
